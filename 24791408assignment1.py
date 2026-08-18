c0b436b8173ab63bae49d5ec657f5acb3eae547e
aidberto <xbertovic@gmail.com>
2026-08-18 16:39:02 +1000

    parser functions and main function

--- 24791408assignment1.py
+++ 24791408assignment1.py
@@ -7,3 +7,30 @@ SPACE = " "
 LAMBDA = "λ"
 GLYPHS = set("+×∸⊤⊥←↑→↓∷Θ")
 VAR_TAIL = LETTERS | DIGITS | set("-")
+
+
+
+class Parser:
+    def __init__(self):
+        self.text = text
+        self.position = 0
+
+    def peek(self):
+        if self.position < len(self):
+            return self.text[self.pos]
+        return ""
+
+    def expect(self):
+
+    def skip_space(self):
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Invalid input")
+        sys.exit(2)
+    try:
+        print("Valid")
+    except error:
+        print("Invalid:", error)
+        sys.exit(1)
