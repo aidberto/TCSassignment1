5cd17f0858b10d22907915ef5aba946e6c969cde
aidberto <xbertovic@gmail.com>
2026-08-18 16:17:47 +1000

    add vars

---
+++ 24791408assignment1.py
@@ -0,0 +1,9 @@
+import sys 
+
+LETTERS = set("abcdefghijklmnopqrstuvwxyz")
+DIGITS = set("0123456789")
+DOT = "."
+SPACE = " "
+LAMBDA = "λ"
+GLYPHS = set("+×∸⊤⊥←↑→↓∷Θ")
+VAR_TAIL = LETTERS | DIGITS | set("-")
